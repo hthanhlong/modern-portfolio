28034a698dcce122e97f39c1fd9c43d0e63f5236
Long Hoang <64834856+hthanhlong@users.noreply.github.com>
2025-03-03 08:24:19 -0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-
-docker stop modern-portfolio-container
-docker rm modern-portfolio-container
-docker rmi modern-portfolio-image
-docker build -t modern-portfolio-image .
-docker run -d -p 3000:3000 --name modern-portfolio-container -d modern-portfolio-image
