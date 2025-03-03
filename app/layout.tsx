7a50b23222d2a1cd19c685b5266861da4cb2c104
hthanhlong <hthanhlong97@gmail.com>
2025-03-03 08:40:49 -0800

    add openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,28 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Long Hoang",
+  metadataBase: new URL("https://hthanhlong97.ca"),
   description: "Software Engineer - Open to work",
   icons: {
     icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Long Hoang",
+    description: "Software Engineer - Open to work",
+    images: [
+      {
+        url: "/images/personal.jpg",
+      },
+    ],
+    siteName: "Long Hoang",
+    locale: "ca_CA",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Long Hoang",
+    description: "Software Engineer - Open to work",
+    images: "/images/personal.jpg",
   },
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -14,35 +14,23 @@ import {
 
 export default function Home() {
   return (
-    <>
-      {/* <head>
-        <meta property="og:title" content="Product Name | Buy Now" />
-        <meta
-          property="og:description"
-          content="High-quality product at the best price. Buy now and get free shipping!"
-        />
-        <meta property="og:image" content="https://yourwebsite.com/image.jpg" />
-        <meta property="og:url" content="https://yourwebsite.com/product" />
-        <meta property="og:type" content="website" />
-      </head> */}
-      <div className="p-4">
-        <Wrapper>
-          <Header />
-          <Hero />
-          <OpenToWork />
-          <Connect />
-          <div className="flex flex-col gap-4 lg:flex-row">
-            <Experience />
-            <PersonalProjects />
-          </div>
-          <div className="flex flex-col gap-4 lg:flex-row">
-            <Map />
-            <MyHobbies />
-            <MyQuote />
-          </div>
-          <Footer />
-        </Wrapper>
-      </div>
-    </>
+    <div className="p-4">
+      <Wrapper>
+        <Header />
+        <Hero />
+        <OpenToWork />
+        <Connect />
+        <div className="flex flex-col gap-4 lg:flex-row">
+          <Experience />
+          <PersonalProjects />
+        </div>
+        <div className="flex flex-col gap-4 lg:flex-row">
+          <Map />
+          <MyHobbies />
+          <MyQuote />
+        </div>
+        <Footer />
+      </Wrapper>
+    </div>
   );
 }
