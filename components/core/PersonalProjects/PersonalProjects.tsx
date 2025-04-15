7bc760dc9df6738d91c044847822cd769955104b
hthanhlong <hthanhlong97@gmail.com>
2025-04-15 11:48:52 -0700

    chore: update ui in personal projects

--- components/core/PersonalProjects/PersonalProjects.tsx
+++ components/core/PersonalProjects/PersonalProjects.tsx
@@ -8,6 +8,7 @@ interface Project {
   title: string;
   image: string;
   live: string;
+  description: string;
   github?: string;
   links?: {
     title: string;
@@ -18,47 +19,35 @@ interface Project {
 const projects: Project[] = [
   {
     id: 1,
-    title: "Code Editor",
-    image: "/images/react.jpeg",
-    live: "https://codepin.hthanhlong97.ca/",
-    github: "https://github.com/hthanhlong/react_code_editor",
-  },
-  {
-    id: 2,
     title: "Nike UI",
     image: "/images/nike.jpeg",
     live: "https://clone.hthanhlong97.ca/",
-    github: "https://github.com/hthanhlong/nike-clone",
+    description: "A clone of the Nike website",
+    links: [
+      {
+        title: "Frontend",
+        url: "https://github.com/hthanhlong/nike-clone",
+      },
+    ],
   },
   {
     id: 3,
     title: "Chat App",
     image: "/images/chat-app.jpeg",
     live: "https://chat-page-long.vercel.app/",
+    description: "A chat app",
     links: [
       {
         title: "Frontend",
-        url: "https://github.com/hthanhlong/chat-page",
+        url: "https://github.com/hthanhlong/chat-app-frontend",
       },
       {
-        title: "Backend",
-        url: "https://github.com/hthanhlong/chat-page-backend",
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "E-commerce",
-    image: "/images/e-commerce.jpeg",
-    live: "https://e-commerce-long.vercel.app/",
-    links: [
-      {
-        title: "Frontend",
-        url: "https://github.com/hthanhlong/microservice-app/tree/main/fe-a5-market",
+        title: "Socket",
+        url: "https://github.com/hthanhlong/chat-app-socket",
       },
       {
-        title: "Backend",
-        url: "https://github.com/hthanhlong/microservice-app",
+        title: "API",
+        url: "https://github.com/hthanhlong/chat-app-backend",
       },
     ],
   },
@@ -67,49 +56,36 @@ const projects: Project[] = [
 export default function PersonalProjects() {
   return (
     <CustomCard title="Personal Projects" className="w-full">
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
         {projects.map((project) => (
           <Card className="w-full text-center p-2 border-0" key={project.title}>
-            <div className="relative group">
-              <Image
-                src={project.image}
-                alt={project.title}
-                width={200}
-                height={200}
-                className="w-full h-full object-cover hover:opacity-80 transition-all duration-300"
-              />
-              <Link
-                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-gray-700 font-bold bg-white px-2 py-1 rounded-lg text-xs hidden group-hover:block hover:opacity-80 transition-all duration-300"
-                href={project.live || ""}
-                target="_blank"
-              >
-                Live
-              </Link>
-            </div>
+            <Image
+              src={project.image}
+              alt={project.title}
+              width={200}
+              height={200}
+              className="w-full h-full object-cover hover:opacity-80 transition-all duration-300 cursor-pointer"
+            />
             <h1 className="text-sm lg:text-xs">{project.title}</h1>
-            {project.links && project.links.length > 0 ? (
-              <div className="flex gap-2">
+            {project.links && project.links.length > 0 && (
+              <div className="flex gap-2 justify-center items-center bg-gray-100 py-1 rounded-lg">
                 {project.links.map((link) => (
                   <Link
                     key={link.title}
                     href={link.url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="w-full lg:text-[10px] text-xs p-1 rounded-xl bg-blue-200 text-gray-700 hover:bg-gray-700 hover:text-white transition-all duration-300"
+                    className="bg-gray-200 p-2 rounded-lg hover:bg-gray-300 transition-all"
                   >
-                    {link.title}
+                    <Image
+                      src="/github.svg"
+                      alt="flag"
+                      width={24}
+                      height={24}
+                    />
                   </Link>
                 ))}
               </div>
-            ) : (
-              <Link
-                href={project.github || ""}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="w-full lg:text-[10px] text-xs p-1 rounded-xl bg-blue-200 text-gray-700 hover:bg-gray-700 hover:text-white transition-all duration-300"
-              >
-                Frontend
-              </Link>
             )}
           </Card>
         ))}
--- components/ui/Timeline/_data/index.ts
+++ components/ui/Timeline/_data/index.ts
@@ -1,26 +1,26 @@
 export const experience = [
   {
     id: 1,
-    title: "Kitchen Staff at Sushi9",
-    description: "12/2023 - Present Full Time",
+    title: "Freelancer",
+    description: "12/2023 - Present",
     location: "Canada",
   },
   {
     id: 2,
     title: "Software Engineer at S3Corp",
-    description: "01/2022 - 08/2023 Full Time",
+    description: "01/2022 - 08/2023",
     location: "Vietnam",
   },
   {
     id: 3,
     title: "Software Engineer at NPTech",
-    description: "01/2021 - 11/2021 Full Time",
+    description: "01/2021 - 11/2021",
     location: "Vietnam",
   },
   {
     id: 4,
     title: "Software Engineer at EliSoft",
-    description: "10/2019 - 12/2020 Full Time",
+    description: "10/2019 - 12/2020",
     location: "Vietnam",
   },
 ];
